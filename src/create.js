35a63cd0c6ae403a5af262b3637eaf4a4740a81d
wendingding <18681537032@163.com>
2020-05-27 10:07:50 +0800

    update ora

--- src/create.js
+++ src/create.js
@@ -2,6 +2,7 @@
 /* 2.导入模块 */
 const axios = require('axios');
 const ora = require('ora');
+const inquirer = require('inquirer');
 
 /* 封装函数获取存放模板信息的数据 */
 async function fetchRepositoryList() {
@@ -15,12 +16,36 @@ module.exports = async(arg) => {
 
     /* [ 'vue-simple-template', 'vue-template', 'easy-webpack-demo' ] */
 
-    let task = ora("开始请求模板文件...");
-    task.start();
+    // let task = ora("开始请求模板文件...");
+    // task.start();
 
     // let data = await fetchRepositoryList();
     // // console.log(data);
     // let names = data.map(item => item.name);
     // console.log(names);
     // setTimeout(spinner.succeed, 2000);
+
+
+    let repository = ['vue-simple-template', 'vue-template', 'easy-webpack-demo'];
+
+    /* 提示选择用来创建项目的模板 */
+    const { repo } = await inquirer.prompt({
+        name: "repo",
+        type: "list",
+        message: "please choice a template to create project !",
+        choices: repository
+    })
+
+    console.log("repo ->", repo)
+        /* 提示选择需要的版本 */
+    let tags = ["1.0.0", "2.0.0", "3.0.1"];
+    const { tag } = await inquirer.prompt({
+        name: 'tag', // 获取选择后的结果
+        type: 'list',
+        message: 'please choices tags to create project',
+        choices: tags,
+    });
+
+    console.log("tag ->", tag)
+
 };
--- src/dist/create.dev.js
+++ src/dist/create.dev.js
@@ -6,6 +6,8 @@
 var axios = require('axios');
 
 var ora = require('ora');
+
+var inquirer = require('inquirer');
 /* 封装函数获取存放模板信息的数据 */
 
 
@@ -33,22 +35,55 @@ function fetchRepositoryList() {
 }
 
 module.exports = function _callee(arg) {
-  var task;
+  var repository, _ref2, repo, tags, _ref3, tag;
+
   return regeneratorRuntime.async(function _callee$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
         case 0:
           console.log("arg", arg);
           /* [ 'vue-simple-template', 'vue-template', 'easy-webpack-demo' ] */
-
-          task = ora("开始请求模板文件...");
-          task.start(); // let data = await fetchRepositoryList();
+          // let task = ora("开始请求模板文件...");
+          // task.start();
+          // let data = await fetchRepositoryList();
           // // console.log(data);
           // let names = data.map(item => item.name);
           // console.log(names);
           // setTimeout(spinner.succeed, 2000);
 
-        case 3:
+          repository = ['vue-simple-template', 'vue-template', 'easy-webpack-demo'];
+          /* 提示选择用来创建项目的模板 */
+
+          _context2.next = 4;
+          return regeneratorRuntime.awrap(inquirer.prompt({
+            name: "repo",
+            type: "list",
+            message: "please choice a template to create project !",
+            choices: repository
+          }));
+
+        case 4:
+          _ref2 = _context2.sent;
+          repo = _ref2.repo;
+          console.log("repo ->", repo);
+          /* 提示选择需要的版本 */
+
+          tags = ["1.0.0", "2.0.0", "3.0.1"];
+          _context2.next = 10;
+          return regeneratorRuntime.awrap(inquirer.prompt({
+            name: 'tag',
+            // 获取选择后的结果
+            type: 'list',
+            message: 'please choices tags to create project',
+            choices: tags
+          }));
+
+        case 10:
+          _ref3 = _context2.sent;
+          tag = _ref3.tag;
+          console.log("tag ->", tag);
+
+        case 13:
         case "end":
           return _context2.stop();
       }
